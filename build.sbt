4d4ab0e70f3160d62aa4a5bd6c0232ed1a26a15b
iwag <iwagame@gmail.com>
2016-10-10 03:06:06 +0900

    Add the integration of evalutions and Task table

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
+  evolutions,
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,16 @@
+# Tasks schema
+
+# --- !Ups
+
+CREATE SEQUENCE task_id_seq;
+CREATE TABLE task (
+	id bigint NOT NULL DEFAULT nextval('task_id_seq'),
+	body varchar(255) NOT NULL,
+	is_complete boolean NOT NULL DEFAULT false
+);
+
+# --- !Downs
+
+DROP TABLE task;
+DROP SEQUENCE task_id_seq;
+
